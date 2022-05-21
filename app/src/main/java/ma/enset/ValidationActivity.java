cc1458892f68faa6ca58e4926f8fa780b262d85e
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-05-21 15:31:49 +0100

    commit

--- app/src/main/java/ma/enset/ValidationActivity.java
+++ app/src/main/java/ma/enset/ValidationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import ma.enset.model.DetailCommande;
@@ -23,11 +25,20 @@ public class ValidationActivity extends AppCompatActivity {
         TextView prixpdetail = findViewById(R.id.prixp_detail);
         TextView qtedetail = findViewById(R.id.qte_detail);
         TextView totaldetail = findViewById(R.id.total_detail);
+        Button ok = findViewById(R.id.ok);
 
         msg.setText("M/Mme "+detailCommande.getNomClient()+" votre commande a été enregistrée avec succès");
         nompdetail.setText(detailCommande.getPlat().getName());
         prixpdetail.setText(detailCommande.getPlat().getPrice()+" DH");
-        qtedetail.setText(detailCommande.getQuantite());
-        totaldetail.setText(detailCommande.getQuantite()*detailCommande.getPlat().getPrice()+" DH");
+       // qtedetail.setText(detailCommande.getQuantite());
+       // totaldetail.setText(detailCommande.getQuantite()*detailCommande.getPlat().getPrice()+" DH");
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
